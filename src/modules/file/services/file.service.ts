8202626534770091aa66e5236f1bd3844443d994
4riouha <4riuha@gmail.com>
2024-10-14 13:06:17 +0330

    .

--- src/modules/file/services/file.service.ts
+++ src/modules/file/services/file.service.ts
@@ -26,7 +26,7 @@ export class FileService {
     const images = await this.fileRepo.find({ where: { type: ILike('image%') } });
     return images.map((img) => ({
       id: img.id,
-      src: `http://${this.configService.get('APP_URL')}}/file/${img.id}`,
+      src: `${this.configService.get('APP_URL')}}/file/${img.id}`,
       name: img.name,
     }));
   }
